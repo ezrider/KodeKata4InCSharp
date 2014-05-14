8693058e455c2e3e23fd43015c9beaade7a5e97d
Michael Beaulieu <michael.e.beaulieu@gmail.com>
2014-05-14 08:13:46 -0600

    First Commit

---
+++ KodeKata4InCSharp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KodeKata4InCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = System.IO.File.ReadAllText(@"C:\Users\Mike\Documents\Projects\KodeKata4InCSharp\Data\weather.dat");
+
+            System.Console.WriteLine("Contents: {0}", text);
+
+            // Keep the console window open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            System.Console.ReadKey();
+        }
+    }
+}
