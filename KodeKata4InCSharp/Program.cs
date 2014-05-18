4e5bd5baf957144da29db0fbacd3dd3a87570963
Michael Beaulieu <michael.e.beaulieu@gmail.com>
2014-05-18 07:37:54 -0600

    Steady Progress
    added regex to determine missing line

--- KodeKata4InCSharp/Program.cs
+++ KodeKata4InCSharp/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace KodeKata4InCSharp
 {
@@ -10,13 +11,41 @@ namespace KodeKata4InCSharp
     {
         static void Main(string[] args)
         {
+            string startupPath = Environment.CurrentDirectory;
+            Console.WriteLine("currennt directory {0}", startupPath);
             string text = System.IO.File.ReadAllText(@"C:\Users\Mike\Documents\Projects\KodeKata4InCSharp\Data\weather.dat");
 
-            System.Console.WriteLine("Contents: {0}", text);
+            string[] lines = System.IO.File.ReadAllLines(@"C:\Users\Mike\Documents\Projects\KodeKata4InCSharp\Data\weather.dat");
+            List<string> newlines = new List<string>(lines);
 
-            // Keep the console window open in debug mode.
-            Console.WriteLine("Press any key to exit.");
-            System.Console.ReadKey();
+            int count = 0;
+            foreach (string line in lines)
+            {
+                if (lines[count].Length == 0)
+                {
+                    newlines.RemoveAt(count);
+                    Console.WriteLine("line length is {0} and {1}", line.Length, lines[count].Length);
+                    count++;
+                }
+
+                // try with linq
+
+                //            goodlines = lines.Where((val,idx) => Length != 0).ToArray();
+
+                // now we can work with our new lines to check the data integrity
+
+
+                string[] line3words = Regex.Split(lines[2], @"\W+");
+                foreach (string word in line3words)
+                {
+                    Console.WriteLine("{0}", word);
+                }
+
+
+                // Keep the console window open in debug mode.
+                Console.WriteLine("Press any key to exit.");
+                System.Console.ReadKey();
+            }
         }
     }
 }
